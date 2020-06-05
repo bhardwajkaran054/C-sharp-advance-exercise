4ca2a1e325d9bd68f4c6e821f2c73748ccbf027c
Karan Bhardwaj <bhardwajkaran054@gmail.com>
2020-06-05 16:03:11 +0530

    Create ExtensionMethod.cs

---
+++ ExtensionMethod.cs
@@ -0,0 +1,41 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max = numbers.Max();
+
+            Console.WriteLine(max);
+
+        }
+    }
+	public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
